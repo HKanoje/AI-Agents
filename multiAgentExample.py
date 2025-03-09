5a310f0e21cdc7e019063fd94fd3b8a394c6eb83
HKanoje <hrithik.kanoje@gmail.com>
2025-03-09 18:47:06 -0400

    added multiagent example

---
+++ multiAgentExample.py
@@ -0,0 +1,21 @@
+from smolagents import CodeAgent, LiteLLMModel, DuckDuckGoSearchTool
+
+model = LiteLLMModel(
+    model_id="ollama/qwen2.5:7b",
+    api_base="http://127.0.0.1:11434",
+    num_ctx=8192,
+)
+
+
+web_agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool()],
+    model=model,
+    name="web_search_agent",
+    description="Runs web searches for you. Give it your query as an argument."
+)
+
+manager_agent = CodeAgent(
+    tools=[], model=model, managed_agents=[web_agent]
+)
+
+manager_agent.run("Who is the CEO of Hugging Face?")
